79dd3e53b715bbe06be7eb259b58b51b709422af
Pravith Chary Kammari <kpravithchary2002@gmail.com>
2026-03-02 21:55:05 +0530

    p1: creating a serializer and CRUD operations

--- react-flask-project/backend/main.py
+++ react-flask-project/backend/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restx import Api, Resource
+from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Recipe
 from exts import db
@@ -11,10 +11,44 @@ db.init_app(app)
 
 api = Api(app, doc = '/docs')
 
+# model (serializer)
+recipe_model = api.model(
+    "Recipe",
+    {
+        "id": fields.Integer,
+        "title": fields.String,
+        "description": fields.String
+    }
+)
+
 @api.route('/hello')
 class HelloWorld(Resource):
     def get(self):
         return {'message': 'Hello World'}
+
+@api.route('/recipes')
+class RecipesResource(Resource):
+    def get(self):
+        """Get all recipes"""
+        pass
+
+    def post(self):
+        """Create a new recipe"""
+        pass
+
+@api.route('/recipes/<int:id>')
+class RecipeResource(Resource):
+    def get(self, id):
+        """Get a recipe by ID"""
+        pass
+
+    def put(self, id):
+        """Update a recipe by ID"""
+        pass
+
+    def delete(self, id):
+        """Delete a recipe by ID"""
+        pass
 
 @app.shell_context_processor
 def make_shell_context():
