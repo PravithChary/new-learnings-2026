add79d7b47d4ca6033398cfecf954e870abb5605
Pravith Chary Kammari <kpravithchary2002@gmail.com>
2026-03-02 22:23:23 +0530

    p1: CRUD operations

--- react-flask-project/backend/main.py
+++ react-flask-project/backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import Recipe
@@ -28,28 +28,49 @@ class HelloWorld(Resource):
 
 @api.route('/recipes')
 class RecipesResource(Resource):
+    @api.marshal_list_with(recipe_model)
     def get(self):
-        """Get all recipes"""
-        pass
+        # ---------- READ
+        """Get all recipes""" 
+        return Recipe.query.all()
 
+    @api.marshal_with(recipe_model)
     def post(self):
+        # ---------- CREATE
         """Create a new recipe"""
-        pass
+        data = request.get_json()
+        new_recipe = Recipe(
+            title=data.get('title'), 
+            description=data.get('description')
+        )
+        new_recipe.save()
+        return new_recipe, 201
 
-@api.route('/recipes/<int:id>')
+@api.route('/recipe/<int:id>')
 class RecipeResource(Resource):
+    @api.marshal_with(recipe_model)
     def get(self, id):
+        # ---------- READ
         """Get a recipe by ID"""
-        pass
+        return Recipe.query.get_or_404(id)
 
+    @api.marshal_with(recipe_model)
     def put(self, id):
+        # ---------- UPDATE
         """Update a recipe by ID"""
-        pass
+        recipe_to_update = Recipe.query.get_or_404(id)
+        recipe = request.get_json()
+        recipe_to_update.update(recipe.get('title'), recipe.get('description'))
+        return recipe_to_update
 
+    @api.marshal_with(recipe_model)
     def delete(self, id):
+        # ---------- DELETE
         """Delete a recipe by ID"""
-        pass
-
+        recipe_to_delete = Recipe.query.get_or_404(id)
+        recipe_to_delete.delete()
+        return recipe_to_delete
+    
 @app.shell_context_processor
 def make_shell_context():
     return {
