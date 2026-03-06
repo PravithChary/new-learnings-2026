df77a2f8bd4b34e8cacb0c5a1caf58ec2bb43ce1
Pravith Chary Kammari <kpravithchary2002@gmail.com>
2026-03-06 10:50:21 +0530

    lld-dp-c: singleton - basic

---
+++ java-design-patterns/creational/src/main/java/com/singleton/SingletonEverydayDemo.java
@@ -0,0 +1,18 @@
+package main.java.com.singleton;
+
+public class SingletonEverydayDemo {
+    public static void main(String args[]) {
+        Runtime singletonRuntime = Runtime.getRuntime();
+        singletonRuntime.gc();
+        System.out.println(singletonRuntime);
+
+        Runtime anotherInstance = Runtime.getRuntime();
+        anotherInstance.gc();
+        System.out.println(anotherInstance);
+
+        if (singletonRuntime == anotherInstance) {
+            System.out.println("They are the same instance!");
+        }
+
+    }
+}
