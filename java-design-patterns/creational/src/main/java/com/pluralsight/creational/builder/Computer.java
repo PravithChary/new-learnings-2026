3d8fa25c1441a4fea237ca9ca6600f8eba566055
Pravith Chary Kammari <kpravithchary2002@gmail.com>
2026-03-06 15:13:01 +0530

    lld-dp-c: builder - builder demo

--- java-design-patterns/creational/src/main/java/com/pluralsight/creational/builder/Computer.java
+++ java-design-patterns/creational/src/main/java/com/pluralsight/creational/builder/Computer.java
@@ -2,44 +2,68 @@ package com.pluralsight.creational.builder;
 
 public class Computer {
 
+    public static class ComputerBuilder {
+        private HDD diskSize;
+        private RAM ramSize;
+        private boolean hasUsbc;
+        private boolean hasGigabitWifi;
+
+        public ComputerBuilder(){
+
+        }
+
+        public Computer build() {
+            return new Computer(this);
+        }
+
+        public ComputerBuilder addRam(RAM ramSize) {
+            this.ramSize = ramSize;
+            return this;
+        }
+
+        public ComputerBuilder addHdd(HDD diskSize) {
+            this.diskSize = diskSize;
+            return this;
+        }
+
+        public ComputerBuilder hasUsbc(boolean hasUsbc) {
+            this.hasUsbc = hasUsbc;
+            return this;
+        }
+
+        public ComputerBuilder hasGigabitWifi(boolean hasGigabitWifi) {
+            this.hasGigabitWifi = hasGigabitWifi;
+            return this;
+        }
+
+    }
+
     private HDD diskSize;
     private RAM ramSize;
-
     private boolean hasUsbc;
     private boolean hasGigabitWifi;
 
-    public Computer() {}
+    public Computer(ComputerBuilder builder) {
+        this.diskSize = builder.diskSize;
+        this.ramSize = builder.ramSize;
+        this.hasUsbc = builder.hasUsbc;
+        this.hasGigabitWifi = builder.hasGigabitWifi;
+    }
 
     public HDD getDiskSize() {
         return diskSize;
-    }
-
-    public void setDiskSize(HDD diskSize) {
-        this.diskSize = diskSize;
     }
 
     public RAM getRamSize() {
         return ramSize;
     }
 
-    public void setRamSize(RAM ramSize) {
-        this.ramSize = ramSize;
-    }
-
     public boolean hasUsbc() {
         return hasUsbc;
     }
 
-    public void setHasUsbc(boolean hasUsbc) {
-        this.hasUsbc = hasUsbc;
-    }
-
     public boolean hasGigabitWifi() {
         return hasGigabitWifi;
-    }
-
-    public void setHasGigabitWifi(boolean hasGigabitWifi) {
-        this.hasGigabitWifi = hasGigabitWifi;
     }
 
 }
--- java-design-patterns/creational/src/main/java/com/pluralsight/creational/builder/ComputerConfigurerDemo.java
+++ java-design-patterns/creational/src/main/java/com/pluralsight/creational/builder/ComputerConfigurerDemo.java
@@ -2,17 +2,17 @@ package com.pluralsight.creational.builder;
 
 public class ComputerConfigurerDemo {
     public static void main(String[] args) {
-        Computer computer = new Computer();
+//        Computer computer = new Computer();
 
-        computer.setDiskSize(HDD.DEFAULT);
-        computer.setRamSize(RAM.DEFAULT);
-        computer.setHasUsbc(true);
-        computer.setHasGigabitWifi(true);
+//        computer.setDiskSize(HDD.DEFAULT);
+//        computer.setRamSize(RAM.DEFAULT);
+//        computer.setHasUsbc(true);
+//        computer.setHasGigabitWifi(true);
 
-        System.out.println(computer.getDiskSize());
-        System.out.println(computer.getRamSize());
-        System.out.println(computer.hasUsbc());
-        System.out.println(computer.hasGigabitWifi());
+//        System.out.println(computer.getDiskSize());
+//        System.out.println(computer.getRamSize());
+//        System.out.println(computer.hasUsbc());
+//        System.out.println(computer.hasGigabitWifi());
 
     }
 }
