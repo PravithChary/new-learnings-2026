5bd751026650938aaeac40763f47086008d39dcb
Pravith Chary Kammari <kpravithchary2002@gmail.com>
2026-03-07 17:43:33 +0530

    lld-dp-s: composite - practice

---
+++ java-design-patterns/structural/src/main/java/com/pluralsight/structural/composite/practice/MenuComponent.java
@@ -0,0 +1,33 @@
+package com.pluralsight.structural.composite.practice;
+
+import java.util.*;
+import java.util.List;
+
+public abstract class MenuComponent {
+
+    String name;
+    String url;
+    List<MenuComponent> menuComponents = new ArrayList<>();
+
+    public MenuComponent add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException("Feature not supported at this level");
+    }
+
+    public MenuComponent remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException("Feature not supported at this level");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public abstract String toString();
+
+    String print(MenuComponent menuComponent) {
+        return name + ": " + url + "\n";
+    }
+}
