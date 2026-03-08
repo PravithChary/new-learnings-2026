4f260266aeeb86b19850ff8a918264a504f0769c
pravith <kpravithchary2002@gmail.com>
2026-03-08 22:41:22 +0530

    lld-dp-b: template - everyday demo

---
+++ java-design-patterns/behavioral/src/main/java/com/pluralsight/behavioral/template/Person.java
@@ -0,0 +1,46 @@
+package com.pluralsight.behavioral.template;
+
+public class Person implements Comparable<Person> {
+    private int age;
+    private String name;
+    private String phoneNumber;
+
+    public Person() {
+
+    }
+
+    public Person(String name, String phoneNumber, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public int compareTo(Person p2) {
+        return Integer.compare(this.getAge(), p2.getAge());
+    }
+}
---
+++ java-design-patterns/behavioral/src/main/java/com/pluralsight/behavioral/template/TemplateEverydayDemo.java
@@ -0,0 +1,33 @@
+package com.pluralsight.behavioral.template;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class TemplateEverydayDemo {
+    private static void printContents(List<Person> people) {
+        for(Person person : people) {
+            System.out.println(person.getName());
+        }
+    }
+
+    public static void main(String[] args) {
+        Person pravith = new Person("Pravith", "99999", 21);
+        Person rohith = new Person("Rohith", "77777", 20);
+        Person ranjith = new Person("Ranjith", "88888", 22);
+
+        List<Person> people = new ArrayList<Person>();
+        people.add(pravith);
+        people.add(ranjith);
+        people.add(rohith);
+
+        System.out.println("Not sorted");
+        printContents(people);
+
+        Collections.sort(people);
+
+        System.out.println("\nSorted by age");
+        printContents(people);
+    }
+}
