573ffe88fdeea2b25f306a4fefcbbcd66b23d32f
pravith <kpravithchary2002@gmail.com>
2026-03-08 13:15:51 +0530

    lld-dp-b: command - practice demo state

--- java-design-patterns/behavioral/src/main/java/com/pluralsight/behavioral/command/practice/CommandDemo.java
+++ java-design-patterns/behavioral/src/main/java/com/pluralsight/behavioral/command/practice/CommandDemo.java
@@ -5,8 +5,13 @@ public class CommandDemo {
     public static void main(String[] args) {
         Light light = new Light();
         Switch lightSwitch = new Switch();
-        Command onCommand = new OnCommand(light);
 
-        lightSwitch.storeAndExecute(onCommand);
+//        Command onCommand = new OnCommand(light);
+//        lightSwitch.storeAndExecute(onCommand);
+
+        Command toggleCommand = new ToggleCommand(light);
+        lightSwitch.storeAndExecute(toggleCommand);
+        lightSwitch.storeAndExecute(toggleCommand);
+        lightSwitch.storeAndExecute(toggleCommand);
     }
 }
